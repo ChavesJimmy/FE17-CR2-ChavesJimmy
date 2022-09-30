9c2a444dc6242045aa38369fc848055946a8d171
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-09-30 21:07:13 +0200

    add function sort

--- Code Review 2/Scripts/CodeReview2.js
+++ Code Review 2/Scripts/CodeReview2.js
@@ -1,10 +1,9 @@
 let Task = JSON.parse(tasks);
-console.log(Task);
-console.log(Task.length);
+
+let newcard = document.getElementById("container");
 
 function print(){
-    let newcard = document.getElementById("container");
-
+    
     for(let i=0; i<Task.length; i++){
     let card =`<div class="card col p-3" style="width: 18rem;">
     <img src="${Task[i].image}" class="card-img-top">
@@ -28,6 +27,7 @@ function print(){
 }
 print();
 
+
 // function to increase the importance
 function increase(i){
     if(Task[i].importance != 5){
@@ -37,6 +37,7 @@ function increase(i){
     }
 
 }
+function addClick() {
 let increaseButtons = document.getElementsByClassName("btn-1")
 
 for (let i = 0; i < increaseButtons.length; i++){
@@ -44,10 +45,12 @@ for (let i = 0; i < increaseButtons.length; i++){
         increase(i);
     })
 }
+}
+addClick();
 
-// function to decrease the importance
+// function to decrease the importance (work but with an error in console, have to check it)
 
-function decrease(i){
+/* function decrease(i){
     if(Task[i].importance != 0){
     Task[i].importance--;
     document.getElementsByClassName("priority-amount")[i].innerHTML = `${Task[i].importance}/5`
@@ -55,14 +58,30 @@ function decrease(i){
     }
 
 }
+function addClick2(i){
 let decreaseButtons = document.getElementsByClassName("btn-2")
 
-// in for loop i=decreaseButtons.length but must be doesn't work if written let i = decreaseButtons.length so i added the value.
-for (let i = 6 ; i < decreaseButtons.length ; i--){
+for (let i = (decreaseButtons.length)-1 ; i < decreaseButtons.length ; i--){
     decreaseButtons[i].addEventListener("click", function(){
         decrease(i);
     })
+}}
+addClick2(); */
+
+
+//function to sort by importance
+document.getElementById("sort").onclick = sortByPriority;
+
+function sortByPriority(){
+    Task.sort((a, b) => b.importance - a.importance);
+    console.table(Task);
+
+    newcard.innerHTML="";
+    print();
+    addClick();
 }
+
+
 
 
 
